4616ea02632b339405e3636e75c648e80a7a1e8e
𝔏𝔬𝔳𝔦𝔰𝔥 𝔊𝔬𝔶𝔞𝔩 <lovishgoyal066@gmail.com>
2025-02-24 20:43:24 +0530

    update

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args) {
+
+        // Best Practies to write variable are - camelCase -> cityName, meaningfull name - city, age
+
+        int _age = 21;
+        float $sal = 10200.32f; // in last f is written in float
+        char gender = 'F';
+        double ppf = 43.23;
+
+
+        System.out.println(_age);
+        System.out.println($sal);
+        System.out.println(gender);
+        System.out.println(ppf); // use for printing on terminal as output
+    }
+}
