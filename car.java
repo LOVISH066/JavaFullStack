1a209ae3ba19d241938578f9815aabb0aa0399e5
𝔏𝔬𝔳𝔦𝔰𝔥 𝔊𝔬𝔶𝔞𝔩 <lovishgoyal066@gmail.com>
2025-02-25 21:34:32 +0530

    add

---
+++ car.java
@@ -0,0 +1,18 @@
+class car {
+
+    String type = "sedan";
+    String name = "virtus";
+    String numPlate = "PB19XX0001";
+
+    public static void main(String[] arg){
+        // object
+        car obj = new car();
+
+        System.out.println(obj.type);
+        System.out.println(obj.name);
+        System.out.println(obj.numPlate);
+
+
+    }
+    
+}
--- variables.java
+++ variables.java
@@ -1,17 +1,14 @@
 public class variables {
+
+    String carName = "b6e";
     public static void main(String[] args) {
+  
+    // object create, object phyical like meomry spcae use but not class use space in moenrmy
+    variables obj21 = new variables(); // object created
 
-        // Best Practies to write variable are - camelCase -> cityName, meaningfull name - city, age
-
-        int _age = 21;
-        float $sal = 10200.32f; // in last f is written in float
-        char gender = 'F';
-        double ppf = 43.23;
+    System.out.println(obj21.carName);
 
 
-        System.out.println(_age);
-        System.out.println($sal);
-        System.out.println(gender);
-        System.out.println(ppf); // use for printing on terminal as output
+
     }
 }
