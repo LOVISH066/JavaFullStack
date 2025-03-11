325c19e6e130bad820e517d157de46a760a70813
𝔏𝔬𝔳𝔦𝔰𝔥 𝔊𝔬𝔶𝔞𝔩 <lovishgoyal066@gmail.com>
2025-03-11 05:32:34 +0530

    ad

---
+++ codes/exception.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class exception {
+    public static void main(String[] args) {
+        //System.out.println(10/0); // divide by zero is infinite
+
+        // exception handling
+        // try-catch throw thorws 
+
+        // try-catch use to handle exception like shown below
+        // File file1  = new File("demo.txt");
+        // Scanner sc2 = new Scanner(file1); shwoing aror that we have not handled the exception
+        // nchecked exception  - - > runtime
+        try
+        {
+            
+        }
+        catch(Exception e)
+        {
+            System.out.println("Hello5");
+        }
+
+        System.out.println("Hellovsd");
+
+    }
+
+    public static int sum(int a, int b){
+
+    }
+    
+}
