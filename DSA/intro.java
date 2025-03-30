6c902b4ff9967cba447b60f5d6f43a3c51c13c5e
𝔏𝔬𝔳𝔦𝔰𝔥 𝔊𝔬𝔶𝔞𝔩 <lovishgoyal066@gmail.com>
2025-03-30 20:22:47 +0530

    da

--- DSA/intro.java
+++ DSA/intro.java
@@ -1,7 +1,7 @@
 package DSA;
 
 public class intro { // class name shiould be same with file name
-    public static void main(String[] args) { //( method ) in java- function
+    public static void main(String[] args) { //( method ) in java- function in other lang
         System.out.println("Hello 2025");
     }
 
