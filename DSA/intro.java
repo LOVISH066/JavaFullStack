bd8e69b5847de54e6e8a14bdcc549dcac157dce6
𝔏𝔬𝔳𝔦𝔰𝔥 𝔊𝔬𝔶𝔞𝔩 <lovishgoyal066@gmail.com>
2025-03-31 22:58:17 +0530

    Ad

--- DSA/intro.java
+++ DSA/intro.java
@@ -1,8 +1,12 @@
 package DSA;
 
 public class intro { // class name shiould be same with file name
-    public static void main(String[] args) { //( method ) in java- function in other lang
+    public static void main(String[] args) { //( method ) in java- function in other lang//
         System.out.println("Hello 2025");
+
+        // class poperties variables defines kya karti ha vho class - methods tell what class will do or kya karti ha uska function kya haa use method kehte haa in java
+        
+
     }
 
 }
