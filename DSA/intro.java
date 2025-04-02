f7fe80556b364da46297827902723788ed7ad1dd
𝔏𝔬𝔳𝔦𝔰𝔥 𝔊𝔬𝔶𝔞𝔩 <lovishgoyal066@gmail.com>
2025-04-02 23:23:45 +0530

    Da

--- DSA/intro.java
+++ DSA/intro.java
@@ -5,7 +5,10 @@ public class intro { // class name shiould be same with file name
         System.out.println("Hello 2025");
 
         // class poperties variables defines kya karti ha vho class - methods tell what class will do or kya karti ha uska function kya haa use method kehte haa in java
-        
+        // variable container jo value store in class of different type or use data type
+        // method --> fucntioning batada haa    
+        // object  - instance of class
+        // always save file with same of class name
 
     }
 
---
+++ DSA/school.java
@@ -0,0 +1,15 @@
+package DSA;
+
+class school {
+
+    // created school class
+    public static void main(String[] args) { // it is compulry method whci hahs to been each clas, program will from here
+        //System.out.println();
+        job(); // method calling
+    }
+
+    public static void job(){ // mine created method
+        System.out.println("My method");
+    }
+    
+}
