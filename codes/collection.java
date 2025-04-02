32f9e02a83e58120cf3daa27d210ac6178acd510
𝔏𝔬𝔳𝔦𝔰𝔥 𝔊𝔬𝔶𝔞𝔩 <lovishgoyal066@gmail.com>
2025-04-02 23:23:09 +0530

    Ad

---
+++ codes/collection.java
@@ -0,0 +1,13 @@
+public class collection {
+    // collection vho calsses me deeal  karta haa
+    // collections storing same typ rof data or ooebjct
+    // autoboxing --> int -> prmt
+    public static void main(String[] args) {
+        int i = 9;
+        Integer a = i; // autoboxing
+        // prm -> int // unboxing
+        // parse int take int value from string string should onykhave vlaue 
+        System.out.println(a);
+        // in wrapper class we only change the first data type leetter to capital like int -> Integer
+    }    
+}
